28904b0e6cd7d88ce2edf1acb376f2de4bf0b332
dayeong <dyboo1346@naver.com>
2024-12-13 03:31:25 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/springboot/bonusbooklet/BonusBookletApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bonusbooklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusBookletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonusBookletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/bonusbooklet/BonusBookletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bonusbooklet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusBookletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
